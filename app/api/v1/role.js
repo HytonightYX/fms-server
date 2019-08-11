3c5c339971701de401b8162438e4e099140ed348
HytonightYX <whosiyuan@qq.com>
2019-08-11 21:11:09 +0800

    feat: 删除角色接口

--- app/api/v1/role.js
+++ app/api/v1/role.js
@@ -1,6 +1,6 @@
 const Router = require('koa-router')
 const router = new Router({prefix: '/v1/role'})
-const {AddRoleValidator} = require('../../validators/validator')
+const {AddRoleValidator, DelRoleValidator} = require('../../validators/validator')
 const {success} = require('../../lib/helper')
 const {
 	User,
@@ -13,6 +13,9 @@ router.get('/test', async ctx => {
 	ctx.body = {test: 'role router is ok'}
 })
 
+/**
+ * 新建角色Role
+ */
 router.post('/add', async ctx => {
 	const v = await new AddRoleValidator().validate(ctx)
 	const role = {
@@ -20,12 +23,26 @@ router.post('/add', async ctx => {
 		code: v.get('body.code'),
 		status: true,
 	}
-	Role.create({
-		name: v.get('body.name'),
-		code: v.get('body.code'),
-		status: true,
+	Role.create(role)
+	success(`${role.name} 创建成功"`)
+})
+
+/**
+ * 删除角色接口
+ * 软删除
+ */
+router.delete('/delete', async ctx => {
+	const v = await new DelRoleValidator().validate(ctx)
+	const role = await Role.findOne({
+		where: {
+			code: v.get('body.code')
+		}
 	})
-	success(`${v.get('body.name')} 创建成功"`)
+
+	if (role) {
+		await role.destroy({force: true})
+		success(`${role.name} 删除成功"`)
+	}
 })
 
 module.exports = router
