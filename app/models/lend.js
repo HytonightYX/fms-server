70b1a3b5d6ce508469aa9e7e06b8b7c758f088bb
HytonightYX <whosiyuan@qq.com>
2019-09-04 17:29:57 +0800

    feat: 修改Lend业务模型

--- app/models/lend.js
+++ app/models/lend.js
@@ -1,29 +1,31 @@
 const {db} = require('../../core/db')
 const {Model, Op, DataTypes} = require('sequelize')
 
-class Borrow extends Model {
+class Lend extends Model {
 
 }
 
-Borrow.init({
+Lend.init({
 	id: {
 		type: DataTypes.INTEGER,
 		primaryKey: true,
 		autoIncrement: true
 	},
-	// 借阅档案类型: 1: SingleFile / 0: Project 的 SubFile
+	// 借阅档案类型: 100: SingleFile / 200: Project 的 SubFile
 	fileType: DataTypes.TINYINT,
+	// 借阅档案类型
+	file_id: DataTypes.INTEGER,
 	// 借阅开始时间
 	start: DataTypes.TIME,
 	// 借阅时长 (记录小时)
 	length: DataTypes.INTEGER,
 	// 借阅人
-	borrower: DataTypes.INTEGER
+	lender: DataTypes.INTEGER
 }, {
 	sequelize: db,
-	tableName: 'borrow'
+	tableName: 'lend'
 })
 
 module.exports = {
-	Borrow
+	Lend
 }
